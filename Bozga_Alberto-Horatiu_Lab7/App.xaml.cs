052c18c4db05305257d70e5e8891d41830285eda
Alberto Bozga <alberto_bozga@yahoo.com>
2023-12-04 23:43:51 +0200

    dd

--- Bozga_Alberto-Horatiu_Lab7/App.xaml.cs
+++ Bozga_Alberto-Horatiu_Lab7/App.xaml.cs
@@ -1,8 +1,28 @@
-﻿namespace Bozga_Alberto_Horatiu_Lab7;
+﻿using System;
+using Bozga_Alberto_Horatiu_Lab7.Data;
+using System.IO;
+using Bozga_Alberto_Horatiu_Lab7.Data;
+
+namespace Bozga_Alberto_Horatiu_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
--- Bozga_Alberto-Horatiu_Lab7/NewPage1.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Bozga_Alberto_Horatiu_Lab7;
-
-public partial class NewPage1 : ContentPage
-{
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
-}
